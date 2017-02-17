b58091a04a934e686ced30adb6fde235a11aa421
mkjordanov <mkjordanov@gmail.com>
2017-02-17 22:36:03 +0200

    Wokring Admin ManageGames page

---
+++ OldBoardGamesNeedLoveToo/OldBoardGamesNeedLoveToo.MVP/Models/AdminGamesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OldBoardGamesNeedLoveToo.Models;
+
+namespace OldBoardGamesNeedLoveToo.MVP.Models
+{
+    public class AdminGamesViewModel : IAdminGamesViewModel
+    {
+        public IEnumerable<Game> Games { get; set; }
+        public IEnumerable<Category> Categories{ get; set; }
+
+        
+    }
+}
--- OldBoardGamesNeedLoveToo/OldBoardGamesNeedLoveToo.Web/Admin/ManageGames.aspx.cs
+++ OldBoardGamesNeedLoveToo/OldBoardGamesNeedLoveToo.Web/Admin/ManageGames.aspx.cs
@@ -5,24 +5,42 @@ using System;
 using WebFormsMvp;
 using WebFormsMvp.Web;
 using OldBoardGamesNeedLoveToo.MVP.CustomEventArgs;
+using OldBoardGamesNeedLoveToo.MVP.Models;
+using OldBoardGamesNeedLoveToo.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace OldBoardGamesNeedLoveToo.Web.Admin
 {
-    [PresenterBinding(typeof(GamesPresenter))]
-    public partial class ManageGames : MvpPage<GamesViewModel>, IGamesView
+    [PresenterBinding(typeof(AdminGamesPresenter))]
+    public partial class ManageGames : MvpPage<AdminGamesViewModel>, IAdminGamesView
     {
-        public event EventHandler DefaultPageInit;
-        public event EventHandler<FilterGamesEventArgs> OnButtonFilterSubmit;
+        public event EventHandler AdminGetAllCategories;
+        public event EventHandler AdminGetAllGames;
+        public event EventHandler<GameDetailsEventArgs> AdminUpdateGames;
 
-        protected void Page_Load(object sender, EventArgs e)
+        public void Admin_GetAllGames()
         {
-            this.DefaultPageInit?.Invoke(sender, e);
+            this.AdminGetAllGames?.Invoke(this, null);
+        }
 
-            if (!IsPostBack)
-            {
-                this.ListViewGames.DataSource = this.Model.Games;
-                this.ListViewGames.DataBind();
-            }
+        public IQueryable<Game> ListViewGames_GetData()
+        {
+            this.AdminGetAllGames?.Invoke(this, null);
+
+            return this.Model.Games.ToList().AsQueryable();
+        }
+
+        public void ListViewGames_UpdateItem(Guid id)
+        {
+            this.AdminUpdateGames?.Invoke(this, new GameDetailsEventArgs(id));
+        }
+
+        public ICollection<Category> ListViewGames_GetCategories()
+        {
+            this.AdminGetAllCategories?.Invoke(this, null);
+
+            return this.Model.Categories.ToList();
         }
     }
 }
